f52a5aab61549f4528bb86203d838a4d20b62d40
lkwan <louie.kwan@gmail.com>
2018-05-25 11:07:33 -0400

    v1.1

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,7 +1,15 @@
-class apache {    File {      owner => 'apache',      group => 'apache',      mode => 0644,    }  }
+class apache {
+    File {
+      owner => 'lkwan',
+      group => 'lkwan',
+      mode => 0644,
+    }
 
-file {'/var/www/html/index.html':    content => "<html><body><h1><a      href='cookbook.html'>Cookbook!      </a></h1></body></html>\n",  
+file {'/var/www/html/index.html':
+    content => "<html><body><h1><a      href='cookbook.html'>Cookbook3!      </a></h1></body></html>\n",  
 }
 
-file {'/var/www/html/cookbook.html':    content =>      "<html><body><h2>PacktPub</h2></body></html>\n",  
+file {'/var/www/html/cookbook.html':    content =>      "<html><body><h2>PacktPub 3</h2></body></html>\n",  
+}
+
 }

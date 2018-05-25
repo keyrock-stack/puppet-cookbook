cc800ec95088f7a0c9c6cbb88b3539a65236512d
lkwan <louie.kwan@gmail.com>
2018-05-25 15:35:40 -0400

    v1.1

--- git.sh
+++ git.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 git add --a; git commit -m "v1.1"; git push
+#git remote add keyrock  https://github.com/keyrock-stack/puppet-cookbook.git
+#git remote add keyrock-stack https://github.com/keyrock-stack/puppet-cookbook.git
+#git push -u keyrock master

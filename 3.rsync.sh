12e440c0f81b6b1a5b62ccd24595b2d3571be050
Louie KWAN <Louie.Kwan@windriver.com>
2018-06-12 17:21:40 -0400

    v1.1

---
+++ 3.rsync.sh
@@ -0,0 +1,3 @@
+rsync -avz /folk/lkwan/public/cgts4/puppet-manifests/src/modules/openstack/manifests/keystone.pp wrsroot@10.10.10.2:/usr/share/puppet/modules/openstack/manifests/keystone.pp
+rsync -avz /folk/lkwan/public/cgts4/puppet-manifests/src/modules/openstack/templates/keystone-defaultregion.erb wrsroot@10.10.10.2:/usr/share/puppet/modules/openstack/templates/keystone-defaultregion.erb
+rsync -avz /folk/lkwan/public/cgts4/puppet-manifests/src/modules/openstack/templates/keystone-systemcontroller.erb wrsroot@10.10.10.2:/usr/share/puppet/modules/openstack/templates/keystone-systemcontroller.erb
